f694c6584701690558baa18a84da51fba46fcfaa
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2025-04-03 03:55:59 +0900

    Tweak demo/test page.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,10 @@
 	import { gg } from '$lib/index.js';
 
 	gg('Hello, gg!!');
+
+	// gg() without arguments outputs a link that causes
+	// vite to open this file in an editor (like VS Code.)
+	gg('The following link should open the source file in your editor:');
 	gg();
 </script>
 
