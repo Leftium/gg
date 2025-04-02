ecdacb0ea3e67639cc0d87ca812adb98bcebeaf5
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2025-04-03 03:19:52 +0900

    Get gg working in demo/test site.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
+import { openInEditorPlugin } from './src/lib/index.js';
+
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), openInEditorPlugin()]
 });
