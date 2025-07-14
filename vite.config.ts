6bed821a1d1ccad45837ad5979375260b06f8ba4
Leftium <381217+Leftium@users.noreply.github.com>
2025-07-14 15:22:40 -0500

    Fix 404 error for .well-known/appspecific/com.chrome.devtools.json.

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,17 @@
+import devtoolsJson from 'vite-plugin-devtools-json';
 import { openInEditorPlugin } from './src/lib/index.js';
-
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), openInEditorPlugin()],
+	plugins: [sveltekit(), openInEditorPlugin(), devtoolsJson()],
 	build: {
 		target: 'es2022' // or 'esnext' for bleeding-edge features
 	},
 	optimizeDeps: {
 		esbuildOptions: {
 			target: 'es2022',
-			supported: {
-				'top-level-await': true
-			}
+			supported: { 'top-level-await': true }
 		}
 	}
 });
