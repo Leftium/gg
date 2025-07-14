245612f2581a4bbfd3f3b637dc9f3ad1c4dd9024
Leftium <381217+Leftium@users.noreply.github.com>
2025-07-14 14:54:43 -0500

    Update Vite build target so top-level await can be used.

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,16 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), openInEditorPlugin()]
+	plugins: [sveltekit(), openInEditorPlugin()],
+	build: {
+		target: 'es2022' // or 'esnext' for bleeding-edge features
+	},
+	optimizeDeps: {
+		esbuildOptions: {
+			target: 'es2022',
+			supported: {
+				'top-level-await': true
+			}
+		}
+	}
 });
